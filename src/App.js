d359ed216ff9a2eb3fa109f13e0769e77db2bcbc
Brandon <taylor_brandon1@yahoo.com>
2022-11-06 10:19:56 -0600

    linter line again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 // Needs to be before FirebaseAuthService
+// eslint-disable-next-line no-unused-vars
 import firebaseApp from './FirebaseConfig'
 import LoginForm from './components/LoginForm';
 import FirebaseAuthService from './FirebaseAuthService';
